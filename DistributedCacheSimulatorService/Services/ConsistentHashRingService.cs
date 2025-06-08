0d4b43b9516fa9edc33909c7d382043096be3399
Emmanuel Omale <emmanuelomale@africhange.com>
2025-06-08 23:55:17 +0100

    Cache node complete imlementation

--- DistributedCacheSimulatorService/Services/ConsistentHashRingService.cs
+++ DistributedCacheSimulatorService/Services/ConsistentHashRingService.cs
@@ -7,7 +7,34 @@ namespace DistributedCacheSimulatorService.Services
     {
         private SortedDictionary<int, CacheNodeService> Ring = new SortedDictionary<int, CacheNodeService>();
 
-        
+        public void AddNode(CacheNodeService node)
+        {
+            int hash = GetHash(node.NodeId);
+            Ring[hash] = node;
+            Console.WriteLine($"Added Node {node.NodeId} to HashRing at {hash}");
+        }
+
+        public void RemoveNode(CacheNodeService node)
+        {
+            int hash = GetHash(node.NodeId);
+            Ring.Remove(hash);
+            Console.WriteLine($"Removed Node {node.NodeId} from HashRing at {hash}");
+        }
+
+        public CacheNodeService? GetNode(string key)
+        {
+            if (Ring.Count == 0) return null;
+
+            int hash = GetHash(key);
+
+            foreach (var kvp in Ring)
+            {
+                if (hash <= kvp.Key)
+                    return kvp.Value;
+            }
+            
+            return Ring.First().Value;
+        }
 
         private int GetHash(string input)
         {
